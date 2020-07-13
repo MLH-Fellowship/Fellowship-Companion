95255a4ae902367b90ac4cd06776d65710779dcd
Alberto Pérez de Rada Fiol <apdrf.94@gmail.com>
2020-07-13 20:21:41 +0200

    Create the main theme

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,10 +1,26 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#5cc8ae',
+      main: '#00be94',
+      dark: '#008a6b',
+    },
+    secondary: {
+      light: '#ffcd6a',
+      main: '#f8ba2b',
+      dark: '#e2a121',
+    },
+  },
+});
+
 const App = () => (
-  <Fragment>
+  <ThemeProvider theme={theme}>
     <CssBaseline />
-  </Fragment>
+  </ThemeProvider>
 );
 
 export default App;

7e90bb8aa4716f8795b91ddf20d083934b4683e1
Lakshya Khatri <lakshyak67@gmail.com>
2020-07-14 21:14:06 +0530

    Add models for events and fellows

--- server/src/groups/models.py
+++ server/src/groups/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Fellow(models.Model):
+    """
+    A model for storing and handling fellows
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    github_handle = models.CharField(max_length=100)
+    avatar_url = models.URLField()
--- server/src/timeline/models.py
+++ server/src/timeline/models.py
@@ -1,3 +1,65 @@
 from django.db import models
+from groups.models import Fellow
 
-# Create your models here.
+
+class Repository(models.Model):
+    """
+    A model to store and handle repositories to which fellows are contributing
+    """
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    fullname = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    url = models.URLField()
+    contributed_loc = models.BigIntegerField()
+
+
+class Issue(models.Model):
+    """
+    A model to keep an eye on the repository's issues
+    """
+    id = models.IntegerField(primary_key=True)
+    number = models.IntegerField()
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True)
+    status = models.CharField(max_length=10)
+    url = models.URLField()
+    user = models.ForeignKey(to=Fellow, on_delete=models.CASCADE)
+    repo = models.ForeignKey(to=Repository, on_delete=models.CASCADE)
+
+
+class PullRequest(models.Model):
+    """
+    A model to keep an eye on the repository's pull requests
+    """
+    id = models.IntegerField(primary_key=True)
+    number = models.IntegerField()
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True)
+    status = models.CharField(max_length=10)
+    url = models.URLField()
+    additions = models.BigIntegerField()
+    deletions = models.BigIntegerField()
+    user = models.ForeignKey(to=Fellow, on_delete=models.CASCADE)
+    repo = models.ForeignKey(to=Repository, on_delete=models.CASCADE)
+
+
+class Event(models.Model):
+    """
+    A model for handling different events occurring under an organization
+    """
+    PR = 'PR'
+    ISSUE = 'IS'
+    CREATE = 'CR'
+    EVENT_TYPE_CHOICES = [
+        (PR, 'pull_request'),
+        (ISSUE, 'issue'),
+        (CREATE, 'create')
+    ]
+    id = models.IntegerField(primary_key=True)
+    time = models.DateTimeField()
+    type = models.CharField(max_length=2, choices=EVENT_TYPE_CHOICES)
+    issue = models.ForeignKey(to=Issue, null=True, blank=True, on_delete=models.CASCADE)
+    pull_request = models.ForeignKey(to=PullRequest, null=True,
+                                     blank=True, on_delete=models.CASCADE)
+    repository = models.ForeignKey(to=Repository, on_delete=models.CASCADE)

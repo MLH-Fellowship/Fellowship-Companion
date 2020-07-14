32582da1bb2417b68010a26b54226eb94f38a803
Alberto Pérez de Rada Fiol <apdrf.94@gmail.com>
2020-07-14 19:52:32 +0200

    Add button to `LinesOfCode` component

--- client/src/components/Home/LinesOfCode.js
+++ client/src/components/Home/LinesOfCode.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 import CountUp from 'react-countup';
 
@@ -15,9 +16,7 @@ const LinesOfCode = () => {
     >
       <Grid container direction="column" alignItems="center" spacing={2}>
         <Grid item>
-          <Typography variant="body1">
-            The Fellows have contributed a total of
-          </Typography>
+          <Typography variant="body1">The Fellows have contributed</Typography>
         </Grid>
         <Grid item>
           <Typography variant="h3" component="h2" color="secondary">
@@ -28,6 +27,11 @@ const LinesOfCode = () => {
           <Typography variant="body1">
             lines of code to Open Source Software projects!
           </Typography>
+        </Grid>
+        <Grid item>
+          <Button variant="contained" color="secondary" href="/contributions">
+            Contributions
+          </Button>
         </Grid>
       </Grid>
     </div>

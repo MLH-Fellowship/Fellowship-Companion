6b7f058703ea65c68f12bb47cea46b6d80e26ace
Lakshya Khatri <lakshyak67@gmail.com>
2020-07-16 01:59:35 +0530

    Add method to fetching repos

--- server/src/populate_db.py
+++ server/src/populate_db.py
@@ -6,6 +6,16 @@
 ####################################################
 ####################################################
 
+from github import Github
+import os
+
+from timeline.models import (
+    Repository,
+    PullRequest,
+    Issue,
+    Event,
+)
+
 if __name__ == '__main__':
     filepath = os.path.join(os.path.dirname(os.path.dirname(
         os.path.abspath(__file__))), 'env', 'credentials.txt'
@@ -16,3 +26,20 @@ if __name__ == '__main__':
         lines = file.readlines()
         username = lines[0][:-1]
         password = lines[1][:-1]
+
+g = Github(username, password)
+org = g.get_organization('MLH-Fellowship')
+
+
+def get_repos():
+    for repo in org.get_repos(type='forks', sort='created'):
+        db_repo = Repository(
+            id=repo.parent.id,
+            name=repo.parent.name,
+            fullname=repo.parent.full_name,
+            description=repo.parent.description,
+            url=repo.parent.url,
+            contributed_loc=0,
+            created_at=repo.parent.created_at,
+        )
+        db_repo.save()

0bafb2976c7739940801b97b5ab65e8c0c030968
Lakshya Khatri <lakshyak67@gmail.com>
2020-07-16 21:05:49 +0530

    Add endpoints for listing repositories

--- server/src/timeline/urls.py
+++ server/src/timeline/urls.py
@@ -1,3 +1,6 @@
 from django.urls import path, include
+from .views import RepositoryListView
 
-urlpatterns = []
+repository_urls = [
+    path('', RepositoryListView.as_view()),
+]
--- server/src/timeline/views.py
+++ server/src/timeline/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework import generics
+from .models import Repository
+from .serializers import RepositorySerializer
 # Create your views here.
+
+
+class RepositoryListView(generics.ListAPIView):
+    queryset = Repository.objects.all()
+    serializer_class = RepositorySerializer

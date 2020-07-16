5310cb9786cb9066a3439b479a8cd7dd98ace57b
Lakshya Khatri <lakshyak67@gmail.com>
2020-07-16 12:41:23 +0530

    Add view for overview

--- server/src/companion/views.py
+++ server/src/companion/views.py
@@ -1,15 +1,42 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.db.models import Sum
+from django.db.models import Q
+from groups.models import GithubUser
+from timeline.models import (
+    Repository,
+    PullRequest,
+    Issue,
+    Event,
+)
 
 
-class GetLOCView(APIView):
+class GetOverview(APIView):
     """
     View to get number of code lines contributed under MLH.
     """
 
     def get(self, request, format=None):
         """
-        Return a list of all users.
+        Return overview of fellowship.
         """
-        response = {'lines': 1234567894546}
+        response = {}
+        response['loc'] = PullRequest.objects.aggregate(Sum('additions'))['additions__sum']
+        fellows = GithubUser.objects.filter(
+            Q(teams__name__icontains="0.6.3") |
+            Q(teams__name__icontains="0.6.2") |
+            Q(teams__name__icontains="0.6.1") |
+            Q(teams__name__icontains="0.5.2") |
+            Q(teams__name__icontains="0.5.1") |
+            Q(teams__name__icontains="0.4.2") |
+            Q(teams__name__icontains="0.4.1") |
+            Q(teams__name__icontains="0.3.2") |
+            Q(teams__name__icontains="0.3.1") |
+            Q(teams__name__icontains="0.2.2") |
+            Q(teams__name__icontains="0.0.2") |
+            Q(teams__name__icontains="0.1.2") |
+            Q(teams__name__icontains="0.1.1")
+        )
+        response['fellows'] = fellows.count()
+        response['projects'] = Repository.objects.all().count()
         return Response(response)
